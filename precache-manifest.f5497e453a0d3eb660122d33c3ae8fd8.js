da23df6d24623773bffa23c5a982596527b7d29c
Daniil <daniil@rivc-pulkovo.ru>
2019-04-26 00:23:41 +0300

    Updates

---
+++ precache-manifest.f5497e453a0d3eb660122d33c3ae8fd8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2578a214ab0a13928b3",
+    "url": "/marshal_store/static/css/main.d19f9286.chunk.css"
+  },
+  {
+    "revision": "d2578a214ab0a13928b3",
+    "url": "/marshal_store/static/js/main.987520ed.chunk.js"
+  },
+  {
+    "revision": "6c3be4146ea2dd48e876",
+    "url": "/marshal_store/static/js/runtime~main.5cfa5aec.js"
+  },
+  {
+    "revision": "06eaaf29c64136f5a145",
+    "url": "/marshal_store/static/css/2.bb3f35ac.chunk.css"
+  },
+  {
+    "revision": "06eaaf29c64136f5a145",
+    "url": "/marshal_store/static/js/2.0aa1f144.chunk.js"
+  },
+  {
+    "revision": "efdffb4a57443b99c56893e74c4cc622",
+    "url": "/marshal_store/static/media/icon.efdffb4a.svg"
+  },
+  {
+    "revision": "0a51e7bba83f6ce6ef65b35a9ba4e0f2",
+    "url": "/marshal_store/static/media/Framework7Icons-Regular.0a51e7bb.woff2"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/marshal_store/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "0cefb5cc6ff6df9509ed193cff79de7c",
+    "url": "/marshal_store/static/media/Framework7Icons-Regular.0cefb5cc.woff"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/marshal_store/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "61bbf26d99172b62fb8eb5b03585db72",
+    "url": "/marshal_store/static/media/Framework7Icons-Regular.61bbf26d.ttf"
+  },
+  {
+    "revision": "d5e7e689e790dd5f60eaa438da65581f",
+    "url": "/marshal_store/static/media/Framework7Icons-Regular.d5e7e689.eot"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/marshal_store/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/marshal_store/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "b0b111a2c99cd8b40075255444e0ac24",
+    "url": "/marshal_store/index.html"
+  }
+];
